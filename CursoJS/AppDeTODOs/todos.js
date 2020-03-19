041a6eb509ccf45d81085caf801579087ff4f70c
laoliveira <laoliveira543@gmail.com>
2020-03-19 20:06:42 -0300

    Adds 'deleteTODO' of the 5th class/module 3

--- CursoJS/AppDeTODOs/todos.js
+++ CursoJS/AppDeTODOs/todos.js
@@ -9,11 +9,19 @@ buttonElement.onclick = addTODO;
 
 function renderTODOs() {
     listElement.innerHTML = '';
+
     for(todo of todos){
         var todoElement = document.createElement('li');
         var todoText = document.createTextNode(todo);
+        var linkElement = document.createElement('a');
+        var linkText = document.createTextNode('Excluir');
+        var todoIndex = todos.indexOf(todo);
 
+        linkElement.appendChild(linkText);
+        linkElement.setAttribute('href', '#');
+        linkElement.setAttribute('onclick', 'deleteTODO(' + todoIndex+ ')');
         todoElement.appendChild(todoText);
+        todoElement.appendChild(linkElement);
         listElement.appendChild(todoElement);
     }
 }
@@ -23,5 +31,10 @@ function addTODO(){
     
     todos.push(todoText);
     inputElement.value = '';
+    renderTODOs();
+}
+
+function deleteTODO(index){
+    todos.splice(index, 1);
     renderTODOs();
 }
